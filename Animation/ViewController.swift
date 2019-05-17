7eebf7a7538fe7598bee2a2c7b22a84345bc946e
Stefandi Glivert <stefandiglivert@gmail.com>
2019-05-17 15:41:33 +0700

    create transform animation

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -10,10 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func resetButton(_ sender: UIButton) {
+        UIView.animate(withDuration: 3) {
+            self.mainView.transform = .identity
+        }
+    }
+    @IBAction func rotatebutton(_ sender: UIButton) {
+        UIView.animate(withDuration: 3) {
+            
+            self.mainView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            //reset animasi keawal
+            self.mainView.transform = .identity
+        }
+    }
+    @IBAction func upsizeButton(_ sender: UIButton) {
+        UIView.animate(withDuration: 3) {
+            //perbesar
+            self.mainView.transform = CGAffineTransform(scaleX: 5, y: 5)
+            //self.mainView.transform = .identity
+        }
+    }
+    @IBAction func fusionButton(_ sender: UIButton) {
+        UIView.animate(withDuration: 3) {
+            //membuat animasi berputar
+            self.mainView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            self.mainView.transform = CGAffineTransform(scaleX: 5, y: 5)
+        }
+    }
+    @IBOutlet weak var mainView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //mengetahui frame awal
+        UIupdate()
+//        UIView.animate(withDuration: 3) {
+//            self.mainView.frame.origin.x = 100
+//            self.mainView.frame.origin.y = 200
+//            self.UIupdate()
+//        }
+       
+        
+        
+        
         // Do any additional setup after loading the view.
     }
+    
 
+    func UIupdate()
+    {
+        print(mainView.frame)
+        print("Nilai x = \(mainView.frame.origin.x)")
+        print("Nilai y = \(mainView.frame.origin.y)")
+        print("Nilai width = \(mainView.frame.width)")
+        print("Nilai height = \(mainView.frame.height)")
+    }
 }
 
